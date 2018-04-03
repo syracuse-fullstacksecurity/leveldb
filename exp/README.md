c6a76cbff7c855e41792ca901697e2457d5558cb
Ju Chen <chenju2k6@gmail.com>
2018-04-02 21:06:20 -0400

    single read/write
    Signed-off-by: Ju Chen <chenju2k6@gmail.com>

--- exp/README.md
+++ exp/README.md
@@ -11,5 +11,16 @@ This experiment compares the storage size for three implementation (unsecure Lev
 Run the below command to generate the graph (storage.ps) in the same directory.
 ```
 ./storage.py
-``` 
+```
+
+### Performance
+
+#### Single Thread
+
+This experiment compares the read/write latency for three implementation (unsecure LevelDB, LPAD and B-tree) in a single thread setting. 
+Run the below command to generate the graph (single_read.ps, single_write.ps) in the same directory.
+```
+./single_read.py
+./single_write.py
+```
 
---
+++ exp/single_read.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from collections import namedtuple
+
+
+n_groups = 2
+
+means_baseline = (524.815,5.713)
+
+means_lpad = (637.837,16.839)
+
+means_btree = (981.21,126.817)
+
+fig, ax = plt.subplots()
+
+index = np.arange(n_groups)
+bar_width = 0.2
+
+opacity = 0.4
+error_config = {'ecolor': '0.3'}
+
+rects1 = ax.bar(index, means_baseline, bar_width,
+                alpha=opacity, color='b',
+                 error_kw=error_config,
+                label='Baseline')
+
+rects2 = ax.bar(index + bar_width, means_lpad, bar_width,
+                alpha=opacity, color='r',
+                 error_kw=error_config,
+                label='LPAD')
+
+rects3 = ax.bar(index + bar_width + bar_width, means_btree, bar_width,
+                alpha=opacity, color='y',
+                 error_kw=error_config,
+                label='B-tree')
+
+ax.set_xlabel('Number of records (16-byte key, 100-byte)')
+ax.set_ylabel('Latency (micro-seconds)')
+ax.set_title('Single thread read latency')
+ax.set_xticks(index + bar_width)
+ax.set_xticklabels(('100 million', '1 million'))
+ax.legend()
+
+#fig.tight_layout()
+#plt.show()
+plt.savefig('single_read.ps')
